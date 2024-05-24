8ff83f67ea42ec3f0660d2a38a6074efe3959b82
Lelin Chakma <109274006+lelinchakma45@users.noreply.github.com>
2024-05-24 19:17:58 +0600

    ded

--- app/src/main/java/com/example/callapp/Fragments/RecentFragment.kt
+++ app/src/main/java/com/example/callapp/Fragments/RecentFragment.kt
@@ -1,13 +1,18 @@
 package com.example.callapp.Fragments
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.NonNull
 import androidx.annotation.Nullable
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.callapp.Adapter.CallLogAdapter
@@ -21,6 +26,10 @@ class RecentFragment : Fragment() {
     private lateinit var adapter: CallLogAdapter
     private lateinit var onItemClickListener: CallLogAdapter.OnCallLogItemClickListener
 
+    companion object {
+        private const val REQUEST_CODE_READ_CALL_LOG = 1
+    }
+
     @Nullable
     override fun onCreateView(@NonNull inflater: LayoutInflater, @Nullable container: ViewGroup?, @Nullable savedInstanceState: Bundle?): View? {
         binding = FragmentRecentBinding.inflate(layoutInflater)
@@ -29,12 +38,20 @@ class RecentFragment : Fragment() {
     }
 
     private fun initComponents() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.READ_CALL_LOG), REQUEST_CODE_READ_CALL_LOG)
+        } else {
+            setupRecyclerView()
+            loadData()
+        }
+    }
+
+    private fun setupRecyclerView() {
         val mLayoutManager = LinearLayoutManager(context)
         binding.recyclerView.layoutManager = mLayoutManager
         binding.recyclerView.itemAnimator = DefaultItemAnimator()
         adapter = CallLogAdapter(requireContext())
         binding.recyclerView.adapter = adapter
-        loadData()
     }
 
     private fun loadData() {
@@ -51,5 +68,20 @@ class RecentFragment : Fragment() {
             }
         }
         adapter.setOnItemClickListener(onItemClickListener)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE_READ_CALL_LOG -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    setupRecyclerView()
+                    loadData()
+                } else {
+                    Toast.makeText(context, "Permission denied to read call logs", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
     }
 }
